a0737f87b4ca7c6176a1df0ca1e551658fc24b80
Oliver Shih <olivershih@gmail.com>
2021-04-18 23:36:01 -0700

    finished role adding function

--- db/company.sql
+++ db/company.sql
@@ -11,17 +11,18 @@ create table roles(
     role_id integer auto_increment primary key,
     title varchar(30) not null,
     salary decimal not null,
-    dept_id integer not null
+    dept_id integer not null,
+    foreign key (dept_id) references departments(dept_id)
 );
 
 create table employees(
     id integer auto_increment primary key,
     first_name varchar(30) not null,
     last_name varchar(30) not null,
-    full_name varchar(60) not null,
     role_id integer not null,
     manager_id integer,
-    manager_name varchar(60)
+    foreign key (role_id) references roles(role_id),
+    foreign key (manager_id) references employees(id)
 );
 
 insert into departments (dept_name) values ("RnD");
@@ -35,18 +36,28 @@ insert into roles (title,salary,dept_id) values ("HR",50000,2);
 insert into roles (title,salary,dept_id) values ("CFO",250000,3);
 insert into roles (title,salary,dept_id) values ("Bookkeeper",40000,3);
 
-insert into employees (first_name,last_name,full_name,role_id) values ("AB","CD","AB CD",1);
-insert into employees (first_name,last_name,full_name,role_id,manager_id,manager_name) values ("EF","GH","EF GH",2,1,"AB CD");
-insert into employees (first_name,last_name,full_name,role_id) values ("IJ","KL","IJ KL",3);
-insert into employees (first_name,last_name,full_name,role_id,manager_id,manager_name) values ("MN","OP","MN OP",4,3,"IJ KL");
-insert into employees (first_name,last_name,full_name,role_id) values ("QR","ST","QR ST",5);
-insert into employees (first_name,last_name,full_name,role_id,manager_id,manager_name) values ("UV","WX","UV WX",6,5,"QR ST");
+insert into employees (first_name,last_name,role_id) values ("AB","CD",1);
+insert into employees (first_name,last_name,role_id,manager_id) values ("EF","GH",2,1);
+insert into employees (first_name,last_name,role_id) values ("IJ","KL",3);
+insert into employees (first_name,last_name,role_id,manager_id) values ("MN","OP",4,3);
+insert into employees (first_name,last_name,role_id) values ("QR","ST",5);
+insert into employees (first_name,last_name,role_id,manager_id) values ("UV","WX",6,5);
 
 select * from employees;
 select * from roles;
 select * from departments;
 
-select first_name,last_name,title,salary,manager_name,dept_name
-from employees
-left join roles on employees.role_id = roles.role_id
-left join departments on roles.dept_id = departments.dept_id;
+-- select c1.id, c1.first_name, c1.last_name, title, salary, concat(c2.first_name," ",c2.last_name) as "Manager",dept_name
+-- from employees c1
+-- left join roles on c1.role_id = roles.role_id
+-- left join employees c2 on c1.manager_id = c2.id
+-- left join departments on roles.dept_id = departments.dept_id;
+
+-- select first_name,last_name,title,salary,manager_name,dept_name
+-- from employees
+-- left join roles on employees.role_id = roles.role_id
+-- left join departments on roles.dept_id = departments.dept_id;
+
+-- select role_id,title,salary,dept_name
+-- from roles
+-- left join departments on roles.dept_id = departments.dept_id;
--- db/index.js
+++ db/index.js
@@ -23,10 +23,38 @@ class DB {
     };
 
     allEmployees() {
-        const queryString = `select first_name,last_name,title,salary,manager_name,dept_name
-        from employees
-        left join roles on employees.role_id = roles.role_id
+        const queryString = `select c1.id, c1.first_name, c1.last_name, title, salary, concat(c2.first_name," ",c2.last_name) as "Manager",dept_name
+        from employees c1
+        left join roles on c1.role_id = roles.role_id
+        left join employees c2 on c1.manager_id = c2.id
         left join departments on roles.dept_id = departments.dept_id;`;
+        return this.connection.query(queryString);
+    };
+    
+    allRoles() {
+        const queryString = `select role_id,title,salary,dept_name
+        from roles
+        left join departments on roles.dept_id = departments.dept_id;`;
+        return this.connection.query(queryString);
+    };
+    
+    allDepts() {
+        const queryString = `select * from departments;`;
+        return this.connection.query(queryString);
+    };
+
+    addDept(result) {
+        const queryString = `insert into departments (dept_name) values ("${result.newDept}");`;
+        return this.connection.query(queryString);
+    };
+    
+    getDeptIdByName(result) {
+        const queryString = `select dept_id from departments where dept_name = "${result}";`;
+        return this.connection.query(queryString);
+    };
+
+    addRole(result,id) {
+        const queryString = `insert into roles (title,salary,dept_id) values ("${result.role}",${result.salary},${id});`;
         return this.connection.query(queryString);
     };
 
